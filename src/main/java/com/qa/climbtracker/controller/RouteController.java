2df669a0f25eb3640e81af23e8dc3cebd9105150
tjayling <tjayling@yahoo.co.uk>
2022-06-01 15:55:23 +0100

    Added Route Controller code

--- src/main/java/com/qa/climbtracker/controller/RouteController.java
+++ src/main/java/com/qa/climbtracker/controller/RouteController.java
@@ -1,5 +1,75 @@
 package com.qa.climbtracker.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.climbtracker.domain.dao.RouteDao;
+import com.qa.climbtracker.domain.dto.RouteDto;
+import com.qa.climbtracker.service.RouteService;
+
+@RestController()
+@RequestMapping("/route")
+@CrossOrigin
+
 public class RouteController {
 
+	private RouteService service;
+
+	@Autowired
+		public RouteController(RouteService service) {
+			this.service = service;
+		}
+
+	@PostMapping("/create")
+	public ResponseEntity<RouteDto> create(@RequestBody RouteDao route) {
+		return new ResponseEntity<>(this.service.create(route), HttpStatus.CREATED);
+	}
+
+	@PostMapping("/createMany")
+	public ResponseEntity<List<RouteDto>> createMany(@RequestBody List<RouteDao> routes) {
+		return new ResponseEntity<>(this.service.createMany(routes), HttpStatus.CREATED);
+	}
+
+	@GetMapping("/readAll")
+	public ResponseEntity<List<RouteDto>> readAll() {
+		return new ResponseEntity<>(this.service.readAll(), HttpStatus.OK);
+	}
+
+	@PutMapping("/update")
+	public ResponseEntity<RouteDto> update(@RequestBody RouteDao route) {
+		return new ResponseEntity<>(this.service.update(route), HttpStatus.ACCEPTED);
+	}
+
+	@DeleteMapping("/delete/{ids}")
+	public ResponseEntity<Boolean> delete(@PathVariable List<Long> ids) {
+		return this.service.delete(ids) ? new ResponseEntity<>(HttpStatus.NO_CONTENT)
+				: new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+
+	@GetMapping("/readBy/id:{id}")
+	public ResponseEntity<List<RouteDto>> readId(@PathVariable Long id) {
+		List<RouteDto> routes = this.service.readId(id);
+		return new ResponseEntity<>(routes, HttpStatus.OK);
+	}
+
+	@GetMapping("/readBy/grade:{grade}")
+	public ResponseEntity<List<RouteDto>> readUserId(@PathVariable String grade) {
+		List<RouteDto> routes = this.service.readGrade(grade);
+		return new ResponseEntity<>(routes, HttpStatus.OK);
+	}
+
+	// id
+	// grade
 }
