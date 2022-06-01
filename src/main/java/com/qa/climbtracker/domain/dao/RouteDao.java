3d2bc5d22b0a1d2540c66e222e094d7f771c6149
tjayling <tjayling@yahoo.co.uk>
2022-06-01 17:40:57 +0100

    Added domain.dao code

--- src/main/java/com/qa/climbtracker/domain/dao/RouteDao.java
+++ src/main/java/com/qa/climbtracker/domain/dao/RouteDao.java
@@ -1,5 +1,22 @@
 package com.qa.climbtracker.domain.dao;
 
-public class RouteDao {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class RouteDao {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String grade;
 }
+
+// id
+// grade
