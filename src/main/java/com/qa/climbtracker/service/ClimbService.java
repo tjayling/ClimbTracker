29627bcaba6187f0edb213b3b8bd2acaea6b5d74
tjayling <tjayling@yahoo.co.uk>
2022-06-01 18:35:28 +0100

    Added service code

--- src/main/java/com/qa/climbtracker/service/ClimbService.java
+++ src/main/java/com/qa/climbtracker/service/ClimbService.java
@@ -1,5 +1,79 @@
 package com.qa.climbtracker.service;
 
-public class ClimbService {
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.qa.climbtracker.domain.dao.ClimbDao;
+import com.qa.climbtracker.domain.dto.ClimbDto;
+import com.qa.climbtracker.repo.ClimbRepo;
+
+@Service
+public class ClimbService {
+	private ModelMapper mapper;
+	ClimbRepo repo;
+
+	public ClimbService(ClimbRepo repo, ModelMapper mapper) {
+		super();
+		this.repo = repo;
+		this.mapper = mapper;
+	}
+	
+	private ClimbDto mapToDto(ClimbDao climb) {
+		return this.mapper.map(climb, ClimbDto.class);
+	}
+	
+	public ClimbDto create(ClimbDao climb) {
+		return this.mapToDto(this.repo.save(climb));
+	}
+	
+	public List<ClimbDto> createMany(List<ClimbDao> climbs) {
+		return this.mapToDto(this.repo.saveAll(climbs).stream().map(this::mapToDto).collect(Collectors.toList()));
+	}
+
+	public List<ClimbDto> readAll() {
+		return this.repo.findAll().stream().map(this::mapToDto).collect(Collectors.toList());
+	}
+	
+	public ClimbDto update(ClimbDao newClimb) {
+		ClimbDao climb = this.repo.findById(newClimb.getId()).get();
+		climb.setUserId(newClimb.getUserId());
+		climb.setRouteId(newClimb.getRouteId());
+		climb.setTimeTaken(newClimb.getTimeTaken());
+		climb.setDateClimbed(newClimb.getDateClimbed());
+		climb.setCompletedClimb(newClimb.getCompletedClimb());
+
+		return this.mapToDto(this.repo.save(climb));
+	}
+	
+	@Transactional
+	public Boolean delete(List<Long> ids) {
+		this.repo.delete(ids);
+		return this.repo.exists(ids).size() == 0 ? true : false;
+	}
+	
+	public List<ClimbDto> readId(Long id) {
+		return this.repo.readId(id).stream().map(this::mapToDto).collect(Collectors.toList());
+	}
+	
+	public List<ClimbDto> readUserId(Long userId) {
+		return this.repo.readUserId(userId).stream().map(this::mapToDto).collect(Collectors.toList());
+	}
+	
+	public List<ClimbDto> readRouteId(Long routeId) {
+		return this.repo.readUserId(routeId).stream().map(this::mapToDto).collect(Collectors.toList());
+	}
+	
+	public List<ClimbDto> readDate(Long userId, Date date) {
+		return this.repo.readDate(userId, date).stream().map(this::mapToDto).collect(Collectors.toList());
+	}
+	
+	public List<ClimbDto> readCompleted(Long userId, Boolean completed) {
+		return this.repo.readCompleted(userId, completed).stream().map(this::mapToDto).collect(Collectors.toList());
+	}
 }
+
--- src/main/java/com/qa/climbtracker/service/UserService.java
+++ src/main/java/com/qa/climbtracker/service/UserService.java
@@ -1,5 +1,61 @@
 package com.qa.climbtracker.service;
 
-public class UserService {
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.qa.climbtracker.domain.dao.UserDao;
+import com.qa.climbtracker.domain.dto.UserDto;
+import com.qa.climbtracker.repo.UserRepo;
+
+@Service
+public class UserService {
+	private ModelMapper mapper;
+	UserRepo repo;
+
+	public UserService(UserRepo repo, ModelMapper mapper) {
+		super();
+		this.repo = repo;
+		this.mapper = mapper;
+	}
+	
+	private UserDto mapToDto(UserDao user) {
+		return this.mapper.map(user, UserDto.class);
+	}
+	
+	public UserDto create(UserDao user) {
+		return this.mapToDto(this.repo.save(user));
+	}
+	
+	public List<UserDto> createMany(List<UserDao> users) {
+		return this.mapToDto(this.repo.saveAll(users).stream().map(this::mapToDto).collect(Collectors.toList()));
+	}
+
+	public List<UserDto> readAll() {
+		return this.repo.findAll().stream().map(this::mapToDto).collect(Collectors.toList());
+	}
+	
+	public UserDto update(UserDao newUser) {
+		UserDao user = this.repo.findById(newUser.getId()).get();
+		user.setGrade(null);
+		return this.mapToDto(this.repo.save(user));
+	}
+	
+	@Transactional
+	public Boolean delete(List<Long> ids) {
+		this.repo.delete(ids);
+		return this.repo.exists(ids).size() == 0 ? true : false;
+	}
+	
+	public List<UserDto> readId(Long id) {
+		return this.repo.readId(id).stream().map(this::mapToDto).collect(Collectors.toList());
+	}
+	
+	public UserDto readEmailAddress(String emailAddress) {
+		return this.repo.readEmailAddress(emailAddress).stream().map(this::mapToDto).collect(Collectors.toList());
+	}
 }
+
