961af9a909330e28e8b4cb8e6a6d180357ba1f07
tjayling <tjayling@yahoo.co.uk>
2022-06-01 14:18:30 +0100

    initial commit

---
+++ src/main/java/com/qa/climbtracker/ClimbTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.qa.climbtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/climbtracker/ClimbTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.climbtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
