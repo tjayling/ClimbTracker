c611a1c85f3f44d585abe13a6e8d89e8fc2ac408
tjayling <tjayling@yahoo.co.uk>
2022-06-01 15:19:49 +0100

    Updated AppConfig and added application property files with h2 and sql database options

--- src/main/java/com/qa/climbtracker/config/AppConfig.java
+++ src/main/java/com/qa/climbtracker/config/AppConfig.java
@@ -1,5 +1,15 @@
 package com.qa.climbtracker.config;
 
-public class AppConfig {
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Configuration
+@Profile({"test", "prod"})
+public class AppConfig {
+	@Bean
+	public ModelMapper mapper() {
+		return new ModelMapper();
+	}
 }
