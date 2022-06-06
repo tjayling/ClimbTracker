b152795633eb195736921c5ceb83f106f756789c
tjayling <tjayling@yahoo.co.uk>
2022-06-06 11:53:57 +0100

    Started coding in home.html and home.js

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -10,15 +10,70 @@ function init() {
   console.log("Username " + username);
   document
     .getElementById(`username-header`)
-    .appendChild(document.createTextNode(username));
+    .appendChild(document.createTextNode(`you are logged in as ${username}`));
 }
 
 function logOut() {
-    deleteCookie(`id`)
-    deleteCookie(`username`);
-    deleteCookie(`first-name`);
-    deleteCookie(`last-name`);
-    window.location.href = "./login.html";
+  deleteCookie(`id`);
+  deleteCookie(`username`);
+  deleteCookie(`first-name`);
+  deleteCookie(`last-name`);
+  window.location.href = "./login.html";
+}
+
+function testData() {
+  let testRoutes = [
+    { grade: `v1` },
+    { grade: `v1` },
+    { grade: `v3` },
+    { grade: `v4` },
+  ];
+
+  let testClimbs = [
+    {
+      user: { id: 1 },
+      route: { id: 1 },
+      timeTaken: `31`,
+      completedClimb: `true`,
+    },
+    {
+      user: { id: 1 },
+      route: { id: 3 },
+      timeTaken: `45`,
+      completedClimb: `true`,
+    },
+    {
+      user: { id: 1 },
+      route: { id: 2 },
+      timeTaken: `12`,
+      completedClimb: `false`,
+    },
+  ];
+
+  fetch(`http://localhost:8080/route/createMany`, {
+    method: `post`,
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(testRoutes),
+  }).then((response) => {
+    if (response.status !== 201) {
+      console.error(`status: ${response.status}`);
+      return;
+    }
+  });
+
+  fetch(`http://localhost:8080/climb/createMany`, {
+    method: `post`,
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(testClimbs),
+  }).then((response) => {
+    if (response.status !== 201) {
+      console.error(`status: ${response.status}`);
+      return;
+    }
+    // response.json().then((data) => {
+    //   logIn();
+    // });
+  });
 }
 
 (function () {
